d6f291372d49ba69ccc1b9fd2e00ac5938d1b7f2
St.Onge <stonge.ms@gmail.com>
2021-07-26 23:05:24 -0700

    end of the day

--- porfolio/src/components/App.js
+++ porfolio/src/components/App.js
@@ -2,12 +2,13 @@ import React, { Component } from "react";
 import Navigation from "./Navigation";
 
 export default class App extends Component {
+    obj = {name: "bottom"};
   render() {
     return (
       <div>
         <Navigation title={"NAV"} />
         Main content
-        <Footer />
+        <Footer footer={obj}/>
       </div>
     );
   }
--- porfolio/src/components/Footer.js
+++ porfolio/src/components/Footer.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export default function Footer({footer}) {
+export default function Footer({obj}) {
     return (
         <div>
-            {footer}
+            {obj.name}
         </div>
     )
 }

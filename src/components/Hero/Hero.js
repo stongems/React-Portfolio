feb60b937929e00240922a4ba7937984296ae997
St.Onge <stonge.ms@gmail.com>
2021-08-03 00:02:33 -0700

    finished project

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -7,10 +7,13 @@ export default function Hero({ title, subtitle, image }) {
     return (<img className={styles.img} src={image.src} alt={image.alt} />)
   }
   return (
-    <div style={{ backgroundColor: "white", height: 500 }}>
+    <>
       <h1>{title}</h1>
+    <br></br>
+    <div style={{ backgroundColor: "white", height: 500, padding:"1rem", display:"flex", flexDirection:"column", justifyContent:"space-around", fontFamily: "Gowun Dodum" }}>
       <p>{subtitle}</p>
       {renderImage()}
     </div>
+    </>
   );
 }

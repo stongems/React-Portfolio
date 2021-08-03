7708cb11ccab5efc6c9f9646fda9ad34bdc17323
St.Onge <stonge.ms@gmail.com>
2021-08-02 23:36:40 -0700

    finished contact page

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -2,15 +2,21 @@ import React, { useState } from "react";
 import Hero from "../../components/Hero/Hero";
 
 export default function Contact() {
-    const [to, setTo] = useState("");
-    const [from, setFrom] = useState("");
-    const [message, setMessage] = useState("");
+  const [to, setTo] = useState("");
+  const [from, setFrom] = useState("");
+  const [message, setMessage] = useState("");
+
+  const submitForm = () => {
+      setTo("");
+      setFrom("");
+      setMessage("");
+  }
   return (
     <div>
       <h1 style={{ marginBottom: 50, textAlign: "center" }}>Contact</h1>
 
       <form
-        onSubmit={() => alert("test")}
+        onSubmit={() => submitForm()}
         style={{
           display: "flex",
           flexDirection: "column",
@@ -19,17 +25,30 @@ export default function Contact() {
       >
         <label>To:</label>
         <br />
-        <input type="text" value={} onChange={} />
+        <input
+          type="text"
+          value={to}
+          onChange={(event) => setTo(event.target.value)}
+        />
         <br />
         <br />
         <label for="from">From:</label>
         <br />
-        <input type="text" value={} onChange={} />
+        <input
+          type="text"
+          value={from}
+          onChange={(event) => setFrom(event.target.value)}
+        />
         <br />
         <br />
         <label>Message:</label>
         <br />
-        <textarea name="message" rows="10" value={} onChange={} />
+        <textarea
+          name="message"
+          rows="10"
+          value={message}
+          onChange={(event) => setMessage(event.target.value)}
+        />
         <br />
         <br />
         <button type="sumbit">Submit</button>

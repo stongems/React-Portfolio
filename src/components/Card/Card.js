55e7e8525666e4eea1537ff63bc6f354a8c0bf96
St.Onge <stonge.ms@gmail.com>
2021-08-07 21:32:57 -0700

    added a component

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,1 +1,17 @@
+import React from 'react'
+import styles from './Card.module.css'
 
+
+export default function Card({image, title, link}) {
+    const renderImage = () => {
+        return image ? <img className={styles.image} src={image.src} alt={image.alt} /> : null;
+    }
+    return (
+        <a href={link} target="_blank" rel="noreferrer" style={{ textDecoration: "none"}}>
+        <div className={styles.card}>
+            {renderImage()}
+            <h1 className={styles.title}>{title}</h1>
+        </div>
+        </a>
+    )
+}
